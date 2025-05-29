59fa5e75d72d2c3f35612d63cf920caf11e02244
Lineax17 <philipp.hartmann04@gmail.com>
2025-05-29 16:21:08 +0200

    add real time with opencv

--- real_time.py
+++ real_time.py
@@ -2,17 +2,21 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-# Pfad zum TFLite-Modell
-TFLITE_PATH = 'models/sign_language_mobilenetv2.tflite'
-IMG_SIZE = 224  # Muss zum Training passen
+# Klassennamen aus Datei laden
+with open('data/class_name.txt', 'r') as f:
+    CLASS_NAMES = [line.strip() for line in f]
 
-# TFLite-Interpreter laden
+for idx, name in enumerate(CLASS_NAMES):
+    print(f'Index {idx}: {name}')
+
+TFLITE_PATH = 'models/sign_language_mobilenetv2.tflite'
+IMG_SIZE = 224
+
 interpreter = tf.lite.Interpreter(model_path=TFLITE_PATH)
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Kamera öffnen
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -20,22 +24,20 @@ while True:
     if not ret:
         break
 
-    # Bild zuschneiden und skalieren
     img = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
     img = img.astype(np.float32) / 255.0
     img = np.expand_dims(img, axis=0)
 
-    # Inference
     interpreter.set_tensor(input_details[0]['index'], img)
     interpreter.invoke()
     output = interpreter.get_tensor(output_details[0]['index'])
     pred = np.argmax(output)
 
-    # Ergebnis anzeigen (hier nur Index, passe für deine Labels an)
-    cv2.putText(frame, f'Klasse: {pred}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+    # Klassennamen statt Index anzeigen
+    cv2.putText(frame, f'Klasse: {CLASS_NAMES[pred]}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
     cv2.imshow('TFLite Echtzeit', frame)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # ESC zum Beenden
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cap.release()
