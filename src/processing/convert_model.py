981111d6d96e1558a6e9e67d0d83c3326c436988
Lineax17 <lineax17@gmail.com>
2026-02-27 13:11:33 +0100

    new models

--- src/processing/convert_model.py
+++ src/processing/convert_model.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 from pathlib import Path
 
-MODEL_PATH = 'models/mobilenetv2_unfreezed.h5'
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+MODEL_PATH = BASE_DIR / "models" / "asl_mobilenetv2_freezed.keras"
 TFLITE_PATH = str(Path(MODEL_PATH).with_suffix('.tflite'))
 
 # Modell laden
